6b1e0513dd0865a861ee8fd4a435d523c3df778a
r0l6nd <rolandroland@protonmail.com>
2022-10-02 21:40:46 +0200

    index.html alap, tomb feltoltve, kírathato

---
+++ js/tomb.js
@@ -0,0 +1,8 @@
+var tomb = [99, 18, 25, 3, 8, 110, 37, 26];
+
+function tombKiir(){
+    let span = document.getElementById("tomb-elemei");
+
+    for(let i=0; i<tomb.length; i++)
+        span.innerHTML += tomb[i] + ", ";
+}
